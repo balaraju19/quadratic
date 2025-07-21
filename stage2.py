67608d5e8733555b9fbf1c9dadea15741b5a4b08
balaraju19 <bpilla2@gitam.in>
2025-07-21 14:36:30 +0530

    Delete stage2.py

--- stage2.py
+++
@@ -1,6 +0,0 @@
-a = float(input("Enter coefficient a: "))
-b = float(input("Enter coefficient b: "))
-c = float(input("Enter coefficient c: "))
-t = float(input("Enter time (t): "))
-T = a * t ** 2 + b * t + c
-print(f"Temperature at {t} hour is {T}°C")
