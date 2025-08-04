87eee4359823206dd10abbdab6aad115ba50d88f
balaraju19 <bpilla2@gitam.in>
2025-08-04 14:54:55 +0530

    Add files via upload

---
+++ stage2.py
@@ -0,0 +1,6 @@
+a = float(input("Enter value for a: "))
+b = float(input("Enter value for b: "))
+c = float(input("Enter value for c: "))
+t = float(input("Enter time (t): "))
+temp = a * t**2 + b * t + c
+print("Temperature at time", t, "is:", temp, "°C")
